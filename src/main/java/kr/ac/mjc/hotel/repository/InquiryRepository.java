692e972d4d703f6b4447359f9d0701d7b3b17a57
backgahee <backpowp0920@naver.com>
2023-12-07 20:16:51 +0900

    Revert "전체 파일 업로드 1124"
    This reverts commit ca5540ca83aeb98e20fc598913d53e771cbefec8.

--- src/main/java/kr/ac/mjc/hotel/repository/InquiryRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.ac.mjc.hotel.repository;
-
-import kr.ac.mjc.hotel.domain.Inquiry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryRepository extends JpaRepository<Inquiry,Long> {
-}
--- src/main/java/kr/ac/mjc/hotel/repository/ReservationRepository.java
+++ src/main/java/kr/ac/mjc/hotel/repository/ReservationRepository.java
@@ -3,9 +3,7 @@ package kr.ac.mjc.hotel.repository;
 import kr.ac.mjc.hotel.domain.Reservation;
 import kr.ac.mjc.hotel.dto.AddReservationRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 
 }
